21d10f75dc4068cdb0c6fa69cbae49442c25f325
Adam Bieszk <adambieszk6@gmail.com>
2024-11-08 21:08:24 +0100

    feat: Add options for MCM

---
+++ src/docs/mod_config_menu/ModConfigMenu.lua
@@ -0,0 +1,23 @@
+---@class ModConfigMenu
+_G.ModConfigMenu = {}
+
+---@alias CallbackID ModCallbacks | string
+
+---Returns the category ID based off of the categoryName provided.
+---@param categoryName string
+---@return number | nil
+function ModConfigMenu.GetCategoryIDByName(categoryName)
+end
+
+---Updates a category with the supplied data.
+---@param categoryName string
+---@param categoryData categoryData
+function UpdateCategory(categoryName, categoryData)
+    
+end
+
+---Changes category info.
+---@param categoryName string
+---@param info string
+function ModConfigMenu.SetCategoryInfo(categoryName, info)
+end
